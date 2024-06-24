4dea76e8837d49c7037fc6ed5959b31eed0e19fb
Austin <austinematurure@gmail.com>
2024-06-24 13:56:49 +0300

    Initial commit

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,13 @@
+import '../css/Navbar.css'
 
 export default function Navbar(){
     return(
-        <></>
+        <>
+        <div className="delivery-banner flex">
+                <p> FREE NATIONWIDE DELIVERY AVAILABLE </p>
+
+        </div>
+        
+        </>
     )
 }
