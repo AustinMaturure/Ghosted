9c91d0810ae3da23be56908c65e5e8424c6d3423
Austin <austinematurure@gmail.com>
2024-06-24 14:35:07 +0300

    Navbar style updates

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,11 +1,21 @@
 import '../css/Navbar.css'
-
+import deliveryImg from '../assets/delivery.svg'
+import closeImg from '../assets/close.svg'
 export default function Navbar(){
     return(
         <>
         <div className="delivery-banner flex">
+      
                 <p> FREE NATIONWIDE DELIVERY AVAILABLE </p>
-
+                <div className="delivery-img-cnt">
+                    <img src={deliveryImg} alt="" srcset="" />
+                </div>
+                <div className="delivery-img-cnt">
+                    <img src={closeImg} alt="" />
+                </div>
+                
+          
+               
         </div>
         
         </>
