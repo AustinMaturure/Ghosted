7e3c366ec9b327013c3f9af369c03ba08b0283ec
Faiz Kirsten <faizkirsten@gmail.com>
2024-06-24 13:21:41 +0200

    Added dummy data

--- Frontend/src/components/ItemDetail.jsx
+++ Frontend/src/components/ItemDetail.jsx
@@ -1,11 +1,10 @@
-import { useParams } from "react-router-dom"
-import { useState, useEffect } from "react"
-import axios from "axios"
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import data from "../data/products.json";
 
 export default function ItemDetail() {
-    const { itemID } = useParams()
+    const { itemID } = useParams();
 
-    return(
-        <></>
-    )
+    return <></>;
 }
