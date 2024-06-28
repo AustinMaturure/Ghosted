333f6ea7639276c31cb059d35d5212c536723f0d
Austin <austinematurure@gmail.com>
2024-06-28 23:07:59 +0300

    Added: Basic Search Functionality

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -5,12 +5,39 @@ import logo from '../assets/logo/logos.svg';
 import search from '../assets/logo/search.svg';
 import menu from '../assets/logo/menu.svg';
 import cart from '../assets/logo/cart1.svg';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   const [visible, setVisibility] = useState(true);
   const [svisible, setsVisibility] = useState(true);
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (query) {
+        setIsLoading(true);
+        try {
+          const response = await fetch(`https://ghosted.pythonanywhere.com/api/search/?query=${query}`);
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          const jsonData = await response.json();
+          setData(jsonData);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        } finally {
+          setIsLoading(false);
+        }
+      } else {
+        setData([]);
+      }
+    };
+
+    fetchData();
+  }, [query]);
 
   return (
     <>
@@ -24,7 +51,7 @@ export default function Navbar() {
           <img src={closeImg} alt="Close" />
         </div>
       </div>
-      
+
       {/* Navbar */}
       <nav className='navbar'>
         <div className="menu-img-cnt">
@@ -47,15 +74,38 @@ export default function Navbar() {
         </div>
       </nav>
 
+
       {/* Search box */}
-      <div className={`search-box ${svisible ? '' : 'search-hide'}`}>
-        <input className='search-ipt' type="text" placeholder='Enter Search Query' />
-        <button className='search-btn' type="submit">
-          <div className="search-img-cnt">
-            <img src={search} alt="Search" />
+      {svisible ? (<>
+        <div className={`search-box ${!svisible ? '' : 'search-hide'}`}>
+          <div className="search">
+            <input
+              className='search-ipt'
+              type="text"
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+              placeholder='Enter Search Query'
+            />
+            <button className='search-btn' type="button" onClick={() => setQuery(query)}>
+              <div className="search-img-cnt">
+                <img src={search} alt="Search" />
+              </div>
+            </button>
           </div>
-        </button>
-      </div>
+        </div><div className="search-hint">
+          {isLoading ? (
+            <p style={{ fontFamily: 'Oswald', color: "#fff" }}>Looking...</p>
+          ) : (
+            data.map((result, index) => (
+              <div className='search-hint-link' key={index}>
+                <p><Link to={`item/${result.name.replace(/ /g, '-')}`}>{result.name}</Link></p>
+              </div>
+            ))
+          )}
+        </div></>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
